0ef879b2a21b3078da13501213932184ee7ca7ca
Pedro <pedro@beagenius.co>
2022-08-07 10:08:33 +0200

    add: init basic db module

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from modules.database import Employee
+from modules.database import LeaveApplication
 
 app = FastAPI()
+
+dbLeave = LeaveApplication()
+dbEmployee = Employee()
 
 
 @app.get("/")
 async def read_root():
+
     return {"message": "Hello World"}
 
 
 @app.post("/leave")
 async def capture_leave():
+
     return {"message": "Hello World"}
