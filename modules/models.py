8893a1258c1cbd191cf84078f006c09dd85a127c
Pedro <pedro@beagenius.co>
2022-08-08 11:30:51 +0200

    add: start date greater than end date validation

--- modules/models.py
+++ modules/models.py
@@ -1,6 +1,8 @@
 from datetime import date
 
+from dateutil.parser import parse
 from pydantic import BaseModel
+from pydantic import root_validator
 
 
 class Employee(BaseModel):
@@ -14,5 +16,11 @@ class Leave(BaseModel):
     employee_pk: str
     start_date: date
     end_date: date
-    days_of_leave = int
+    days_of_leave: int
     status: str = "New"
+
+    @root_validator(pre=True)
+    def validate_is_before_end_date(cls, leave):
+        if parse(leave["start_date"]) > parse(leave["end_date"]):
+            raise ValueError("start date must be before end date")
+        return leave
