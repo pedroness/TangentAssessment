de1dcecc69889e5e75a921f013e3bd94d9e55579
Pedro <pedro@beagenius.co>
2022-08-08 11:28:28 +0200

    add: sqllite model insert

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
 from modules.database import Employees
 from modules.database import LeaveApplications
-from modules.models import Employee
 from modules.models import Leave
 
 app = FastAPI()
 dbLeave = LeaveApplications()
 dbEmployee = Employees()
 
-Employee
-
 
 @app.get("/")
 async def read_root():
-
     return {"message": "Hello World"}
 
 
 @app.post("/leave")
 async def capture_leave(leave: Leave):
-
-    return {"message": "Hello World", "leave": leave}
+    dbLeave.insert(
+        (
+            leave.employee_pk,
+            leave.start_date,
+            leave.end_date,
+            leave.days_of_leave,
+            leave.status,
+        )
+    )
+    return leave
