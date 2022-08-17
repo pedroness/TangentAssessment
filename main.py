0d83f0c80d50e372a108a22136ea8aa10c2de4fd
Pedro <pedro@beagenius.co>
2022-08-17 11:16:50 +0200

    edit: Redocs url

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from modules.models import Employee
 
 app = FastAPI(
     docs_url="/api/v1/docs",
-    redoc_url="/api/v1/docs",
+    redoc_url="/api/v1/redocs",
     title="Leave Application API",
     description="Tangent Solutions Interview Assignment, Rest API for employees to log their leave",
     version="1.0",
